cf54f0008e600ec97d37a37a946ea265e991f585
Kowenjko <74559496+Kowenjko@users.noreply.github.com>
2022-12-05 10:54:43 +0200

    fix router

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,8 @@ import WhoView from '../views/WhoView.vue'
 import 'animate.css'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
+  mode: 'history',
   routes: [
     {
       path: '/',
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/test-the-first-the-last/',
+  base: '/mytest-the-first-the-last/',
   plugins: [vue()],
   resolve: {
     alias: {
